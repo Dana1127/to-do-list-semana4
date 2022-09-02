ff7d0188e23dae2174daa035bd672f8a4d7c2bac
Dana1127 <danag20027@gmail.com>
2022-09-01 23:14:51 -0500

    Cambios finales

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 const tasks = [
-    {text: 'Ir a la U', done: true},
-    {text: 'Tarea web', done: false}
-]  
+    { text: "Lectura de investigación en diseño", done: false },
+    { text: "Tarea de Web", done: true },
+    { text: "Terminar landing page en Figma", done: true },
+    { text: "Adelantar de la Electiva", done: false },
+    { text: "Leer para HCI", done: false },
+    { text: "Revisar Intu", done: false },
+    { text: "Organizar Teclado HCI", done: false }
+  ];
+  
+  ///// Selector para cada sección de lista
+  const allTasksHtml = document.getElementById("all");
+  const pendingHtml = document.getElementById("pending");
+  const completedHtml = document.getElementById("completed");
+  
 
-const doneTasks = tasks.filter((task)=> task.done) //task.done ya se sobreentiende como done = true
-const pendingTasks = tasks.filter((task)=> !task.done)
+  function createTasks() {
+  
+    allTasksHtml.innerHTML = "";
+    pendingHtml.innerHTML = "";
+    completedHtml.innerHTML = "";
+  
+    tasks.forEach((task) => {
+      const elem = document.createElement("li");
+      elem.textContent = task.text;
+      elem.className = task.done ? "checked" : "unchecked";
+      elem.addEventListener("click", () => toggleCompletedTask(task));
+  
+      const cloneElem = elem.cloneNode(true);
+      cloneElem.addEventListener("click", () => toggleCompletedTask(task));
+      
+      allTasksHtml.append(elem);
+      
+      if (task.done) {
+        completedHtml.append(cloneElem);
+      } else {
+        pendingHtml.append(cloneElem);
+      }
+    });
+  }
+  
+  createTasks();
+  // Se crea método para llamar al event listener
+  function toggleCompletedTask(task) {
+    task.done = !task.done;
 
-tasks.forEach((task) => {
-const element = document.createElement('li')
-element.className = task.done ? 'checked' : 'unchecked'
-})
-
-createTasks();
+    
+    createTasks();
+  }
